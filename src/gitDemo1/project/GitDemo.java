d8dace0be49e81a40c56f4104b27200904d1d2bf
kumar <kumar@RK-1033>
2020-02-03 16:54:51 +0530

    First commit

---
+++ src/gitDemo1/project/GitDemo.java
@@ -0,0 +1,16 @@
+package gitDemo1.project;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		boolean motion = false;
+		
+		if (motion == true) {
+			System.out.println("Stop!");
+		} else {
+			
+		}
+
+	}
+
+}
