c03f228df4f9dd5f478b0c05b09fb6773089a53c
kumar <kumar@RK-1033>
2020-02-03 17:53:06 +0530

    commiting GitDemo3 to branch3

---
+++ src/gitDemo1/project/GitDemo3.java
@@ -0,0 +1,16 @@
+package gitDemo1.project;
+
+public class GitDemo3 {
+
+	public static void main(String[] args) {
+		boolean rest = true;
+		
+		if (rest == true) {
+			System.out.println("Move!");
+		} else {
+			System.out.println("Stop!");
+		}
+
+	}
+
+}
