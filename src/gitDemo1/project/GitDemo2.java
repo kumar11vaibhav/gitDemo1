d435299d8faf113d8abdb3a6b50405c8533d7a3b
kumar <kumar@RK-1033>
2020-02-03 17:46:40 +0530

    Commiting the incomplete GitDemo2.java

--- src/gitDemo1/project/GitDemo2.java
+++ src/gitDemo1/project/GitDemo2.java
@@ -3,12 +3,12 @@ package gitDemo1.project;
 public class GitDemo2 {
 
 	public static void main(String[] args) {
-		boolean motion = false;
+		boolean rest = true;
 		
-		if (motion == true) {
-			System.out.println("Stop!");
+		if (rest == true) {
+			System.out.println("Move!");
 		} else {
-			
+			System.out.println("Stop!");
 		}
 
 	}
