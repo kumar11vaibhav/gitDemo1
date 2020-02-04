a1f0b17a359505ce708b8b51908347bf2eecd331
kumar11vaibhav <baibhavhckrnarayan@gmail.com>
2020-02-04 18:37:41 +0530

    change2

--- src/gitDemo1/project/GitDemo4.java
+++ src/gitDemo1/project/GitDemo4.java
@@ -6,7 +6,7 @@ public class GitDemo4 {
 		boolean rest = true;
 		
 		if (rest == true) {
-			System.out.println("Moving1");
+			System.out.println("Moving3");
 		} else {
 			System.out.println("Stop!");
 		}
